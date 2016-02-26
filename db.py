0b20ba23113dd0b1696a7addccc76f8485bf7ef5
Chris Rodgers <xrodgers@gmail.com>
2016-02-25 19:51:23 -0500

    adding new mice

--- db.py
+++ db.py
@@ -67,7 +67,8 @@ def getstarted():
         'KF37', 'KM38', 'KM39', 'KF40', 'KF41', 'KF42', 'KM43', 'KM44', 'KM45',
         'KF46', 'KF47', 'KF48', 'KM49', 'KM50', 'KM51', 'KM52', 'KM53',
         'KM54', 'KF57', 'KF58', 'KF59', 'KF60', 'KF61', 'KF62',
-        'KM63', 'KM64', 'KM65', 'KF69', 'KF71', 'KF72', 'KF73', 'KF74',]
+        'KM63', 'KM64', 'KM65', 'KF69', 'KF71', 'KF72', 'KF73', 'KF74',
+        'KF75', 'KF76', 'KF77']
     
     res['rigs'] = ['L0', 'L1', 'L2', 'L3', 'L5', 'L6', 'B1', 'B2', 'B3', 'B4']
     
@@ -77,12 +78,11 @@ def getstarted():
         }
 
     res['cohorts'] = [
-        ['KM52', 'KF60', 'KF61', 'KF62'],
-        ['KM63', 'KM64', 'KM65'],
-        ['KF69', 'KF71', 'KF72', 'KF73', 'KF74'],
+        ['KF60', 'KF61', 'KF62', 'KM63', 'KM64', 'KM65'],
+        ['KF72', 'KF73', 'KF74', 'KF75', 'KF76', 'KF77'],
         ]
     
-    res['active_mice'] = np.sum(res['cohorts'])
+    res['active_mice'] = list(np.concatenate(res['cohorts']))
 
     # Known mice
     assert np.all([alias_val in res['mice'] 
