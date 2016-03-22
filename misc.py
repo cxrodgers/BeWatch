ac60b3600af0c3e8b5513f16ccb340272849ecee
Chris Rodgers <xrodgers@gmail.com>
2016-03-21 22:50:37 -0400

    doc

--- misc.py
+++ misc.py
@@ -1,4 +1,37 @@
-""""Module for miscellaneous BeWatch stuff"""
+""""Module for miscellaneous BeWatch stuff
+
+For example, stuff like extracting lick times or choice times.
+TrialSpeak shouldn't depend on stuff like that.
+
+
+    # Also get the pldf and use that to get lick times
+    ldf = ArduFSM.TrialSpeak.read_logfile_into_df(bdf.loc[idx, 'filename']) 
+    
+    # Get the lick times
+    lick_times = ArduFSM.TrialSpeak.get_commands_from_parsed_lines(ldf, 'TCH')
+    
+    # Group them by trial number and lick type and extract times
+    tt2licks = lick_times.groupby(['trial', 'arg0']).groups
+    for (trial, lick_type) in tt2licks:
+        tt2licks[(trial, lick_type)] = \
+            ldf.loc[tt2licks[(trial, lick_type)], 'time'].values / 1000.
+    
+    # Get response window time as first transition into response window
+    state_change_df = ArduFSM.TrialSpeak.get_commands_from_parsed_lines(
+        ldf, 'ST_CHG2')
+    rwin_open_times = my.pick_rows(state_change_df, 
+        arg1=state_name2num['RESPONSE_WINDOW'])
+    rwin_open_times_by_trial = rwin_open_times.groupby(
+        'trial').first()['time'] / 1000.
+    
+    # Get choice time as first transition out of response window
+    state_change_df = ArduFSM.TrialSpeak.get_commands_from_parsed_lines(
+        ldf, 'ST_CHG2')
+    rwin_close_times = my.pick_rows(state_change_df, 
+        arg0=state_name2num['RESPONSE_WINDOW'])
+    rwin_close_times_by_trial = rwin_close_times.groupby(
+        'trial').first()['time'] / 1000.
+"""
 import BeWatch
 import ArduFSM
 import numpy as np
